e7e6e1a3d425360151637ea879fb6bce73103914
tomas-20 <tacuna20@stuy.edu>
2022-01-23 22:20:27 -0500

    made it so that the client exits when nothing is entered

--- basic_server.c
+++ basic_server.c
@@ -14,14 +14,16 @@ int main() {
   int to_client;
   int from_client;
 
-  from_client = server_handshake( &to_client );
-
   char line[1000];
   while (1) {
-    read(from_client, line, sizeof line);
-    printf("just converted [%s]", line);
-    capitalize(line);
-    printf(" to [%s]\n", line);
-    write(to_client, line, sizeof line);
+    from_client = server_handshake( &to_client );
+    do {
+      read(from_client, line, sizeof line);
+      printf("just converted [%s]", line);
+      capitalize(line);
+      printf(" to [%s]\n", line);
+      write(to_client, line, sizeof line);
+    } while (*line);
   }
+  return 0;
 }
